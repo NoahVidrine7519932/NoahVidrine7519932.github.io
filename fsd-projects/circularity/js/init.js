68a6ab351ee6b1270695e72566a1de4f5a1f590f
NoahVidrine7519932 <nv7519932@lpssonline.com>
2026-03-30 18:22:10 +0000

    todo 2 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -99,8 +99,36 @@ game.checkCirclePosition(circles[ 3 ])
 
 game.checkCirclePosition(circles[ 4 ])
 
+game.checkCirclePosition(circles[ 0 ])
+
+game.checkCirclePosition(circles[ 1 ])
+
+game.checkCirclePosition(circles[ 2 ])
+
+game.checkCirclePosition(circles[ 3 ])
+
+game.checkCirclePosition(circles[ 4 ])
             // TODO 8 / TODO 9 : Iterate over the array
-           
+           physikz.updatePosition(circles[ 0 ])
+
+physikz.updatePosition(circles[ 1 ])
+
+physikz.updatePosition(circles[ 2 ])
+
+physikz.updatePosition(circles[ 3 ])
+
+physikz.updatePosition(circles[ 4 ])
+
+ for (var i = 0; i < circles.length; i++) {
+
+               physikz.updatePosition(circles[i]);
+
+               game.checkCirclePosition(circles[i]);
+
+
+}
+
+
             
         }
     
